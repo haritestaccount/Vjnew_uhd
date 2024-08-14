60fe3a37b45780bebffb3d86eefa7efe682c33a3
manovani <149648920+manovani@users.noreply.github.com>
2024-08-14 22:53:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gopinath2483:UHD2024@cluster143new.a3f8e.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mrprince24gopriya:Cluster143@cluster143.c5rrzgj.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "UHDTamilbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'UHDcollection')
 
